cd29c39c707f2ef269626f32a0d8fa758b7098a0
aya <aya.gamal71994@gmail.com>
2025-06-01 12:39:22 +0200

    edit

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
-const jsonServer = require('json-server')
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
 
-const server = jsonServer.create()
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
 
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('/api', router)
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to port
+server.listen(3000, () => {
+ console.log("JSON Server is running");
+});
 
+// Export the Server API
+module.exports = server;
